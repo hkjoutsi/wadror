f75ec43d11c3da14baa787189ab4f591b6b722e9
Heta Joutsiniemi <heta.joutsiniemi@helsinki.fi>
2015-01-24 13:18:59 +0200

    Viikko 2, tehtävä 6w

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,5 +4,8 @@ class Beer < ActiveRecord::Base
 
     def average_rating
         ratings.average(:score).to_s
+        #n = ratings.count
+        #s = ratings.map { |r| r.score}.inject (0) { |sum, i| sum + i }
+        #s/n
     end
 end
